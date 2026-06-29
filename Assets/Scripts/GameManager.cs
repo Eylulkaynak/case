6e298b0e35b12d63c2fc6d776e40f59068c24023
Eylul Kaynak <kaynakeylul.00@gmail.com>
2026-06-30 00:11:15 +0300

    düşman,sağlık,skor eklendi son güncellemeler yapıldı

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,24 @@
 using UnityEngine;
-using TMPro;
-using System.Collections;
+using TMPro; 
 
 public class GameManager : MonoBehaviour
 {
+    // Singleton referansı
     public static GameManager Instance;
 
+    [Header("Skor Ayarları")]
     public int score = 0;
-    public TMP_Text scoreText;
-
-    [Header("Enemy Spawn Settings")]
-    public GameObject enemyPrefab;
-    public Transform enemySpawnPoint;
-    public float respawnDelay = 2f;
+    public int highScore = 0; 
+    
+    public TextMeshProUGUI scoreText; 
 
     private void Awake()
     {
+        // Singleton Yapısının Kurulumu
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject); 
         }
         else
         {
@@ -28,40 +28,62 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        score = PlayerPrefs.GetInt("SavedScore", 0);
-        UpdateScoreUI();
-    }
-    public void EnemyDied()
-    {
-        AddScore(1);
-        StartCoroutine(RespawnEnemy());
-    }
-
-    private IEnumerator RespawnEnemy()
-    {
-        yield return new WaitForSeconds(respawnDelay);
-
-        Instantiate(enemyPrefab, enemySpawnPoint.position, enemySpawnPoint.rotation);
-    }
-
-    public void AddScore(int amount)
-    {
-        score += amount;
-        PlayerPrefs.SetInt("SavedScore", score);
-        PlayerPrefs.Save();
+        // Oyun başladığında eski rekoru cihazın hafızasından çek
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
         UpdateScoreUI();
     }
 
-    public void ResetScore()
-{
-    score = 0;
-    PlayerPrefs.SetInt("SavedScore", score);
-    PlayerPrefs.Save();
-    UpdateScoreUI();
-}
+    private void Update()
+    {
+        // TEST İÇİN: R tuşuna basınca hem rekoru hem normal skoru sıfırlar
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetHighScore();
+            ResetCurrentScore();
+        }
+    }
 
+    // Skoru artıran metod
+    public void AddScore(int points)
+    {
+        score += points;
+
+        // Yeni rekor kırıldı mı kontrolü
+        if (score > highScore)
+        {
+            highScore = score; 
+            
+            // Yeni rekoru cihaz hafızasına kaydet
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save(); 
+        }
+
+        UpdateScoreUI();
+    }
+
+    // Mevcut skoru sıfırlar (Karakter ölünce veya yeni bölüme geçince çağırılabilir)
+    public void ResetCurrentScore()
+    {
+        score = 0;
+        UpdateScoreUI();
+    }
+
+    // Rekoru (High Score) tamamen sıfırlar ve hafızadan siler
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore"); 
+        highScore = 0;                      
+        UpdateScoreUI();
+        Debug.Log("En yüksek skor hafızadan silindi!");
+    }
+
+    // UI Text'ini güncelleyen metod
     private void UpdateScoreUI()
     {
-        scoreText.text = "Score: " + score;
+        if (scoreText != null)
+        {
+            // Artık ekranda hem skoru hem rekoru yan yana gösterecek
+            scoreText.text = "Skor: " + score + " | Rekor: " + highScore;
+        }
     }
 }
